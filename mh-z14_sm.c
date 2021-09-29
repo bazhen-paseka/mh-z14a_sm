ceea4e6170c2e2bd0b69217b586696e8a312fce8
Бажен Левковець <bazhen.paseka@gmail.com>
2021-09-29 17:58:39 +0300

    deep refactoring

--- mh-z14_sm.c
+++ mh-z14_sm.c
@@ -84,43 +84,34 @@
 **************************************************************************
 */
 
-	void MH_Z14A_Init(void)
-	{
-		char UartBuff[100];
-		sprintf(UartBuff,"\r\nCO2-meter on MH-Z14A sensor\r\nUART1 for debug Start\r\n");
-		HAL_UART_Transmit(&huart1, (uint8_t *)UartBuff, strlen(UartBuff), 100);
+void MH_Z14A_Init(void)	{
+	char UartBuff[100];
+	sprintf(UartBuff,"\t CO2-meter on MH-Z14A sensor\r\n\t UART1 for debug started.\r\n");
+	HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)UartBuff, strlen(UartBuff), 100);
 
-		// RingBuffer variables
+	RingBuffer_DMA_Init(&rx_buffer2, &hdma_usart2_rx, co2_circular_buffer, CO2_BUFFER_SIZE);
+	HAL_UART_Receive_DMA(MH_Z14A_UART, co2_circular_buffer, CO2_BUFFER_SIZE);
+}
+//************************************************************************
 
+uint32_t MH_Z14A_Main(void) {
+	char DataChar[100];
+	sprintf(DataChar,"ack MH-Z14A... ");
+	HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar, strlen(DataChar), 100);
 
+	uint8_t CO2_Packet_Write[9] = {0xff,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
+	HAL_UART_Transmit(MH_Z14A_UART, CO2_Packet_Write, 9, 100);
+	HAL_Delay(1000);
+	uint32_t current_CO2_u32 = CO2_Read();
 
-		/* Array for DMA to save Rx bytes */
+	sprintf(DataChar,"CO2: %d ppm\r\n", (int)current_CO2_u32);
+	HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar, strlen(DataChar), 100);
 
-		RingBuffer_DMA_Init(&rx_buffer2, &hdma_usart2_rx, co2_circular_buffer, CO2_BUFFER_SIZE);
-		HAL_UART_Receive_DMA(&huart2, co2_circular_buffer, CO2_BUFFER_SIZE);
+	return current_CO2_u32;
+}
+//************************************************************************
 
-	}
-
-	uint32_t MH_Z14A_Main(void)
-	{
-		char DataChar[100];
-		sprintf(DataChar,"ack MH-Z14A... ");
-		HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
-
-		uint8_t CO2_Packet_Write[9] = {0xff,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79};
-		HAL_UART_Transmit(&huart2, CO2_Packet_Write, 9, 100);
-		HAL_Delay(1000);
-		uint32_t current_CO2_u32 = CO2_Read();
-
-//		sprintf(DataChar,"CO2: %d ppm\r\n", (int)current_CO2_u32);
-//		HAL_UART_Transmit(&huart1, (uint8_t *)DataChar, strlen(DataChar), 100);
-
-		return current_CO2_u32;
-	}
-
-
-uint32_t CO2_Read(void)
-{
+uint32_t CO2_Read(void) {
 	uint32_t co2_u32 = 0;
 	char DataChar_UART[100];
 	// if something go wrong with DMA - just restart receiving
@@ -132,21 +123,21 @@ uint32_t CO2_Read(void)
 	uint32_t rx_count = RingBuffer_DMA_Count(&rx_buffer2);
 
 	sprintf(DataChar_UART,"uart_recieve %d byte\r\n",(int)rx_count);
-		HAL_UART_Transmit(&huart1, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
+		HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
 
 	while (rx_count--)
 		{
 		uint8_t c = RingBuffer_DMA_GetByte(&rx_buffer2);
-
-		sprintf(DataChar_UART,"%d ", (int)c );	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
-
+		sprintf(DataChar_UART,"%d ", (int)c );
+		HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
 		cmd_co2_char[icmd_co2_u32++] = c;
 		HAL_Delay(10);
 		}// while (rx_count--)
 	co2_u32 = cmd_co2_char[2] * 0xff +cmd_co2_char[3];
 
 	uint8_t CheckSum = getCheckSum(cmd_co2_char);
-	sprintf(DataChar_UART,"\r\ncheckSum: %d\r\n", (int)CheckSum );	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
+	sprintf(DataChar_UART,"\r\ncheckSum: %d\r\n", (int)CheckSum );
+	HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
 
 	if (cmd_co2_char[8] == CheckSum)
 		sprintf(DataChar_UART,"checkSum Ok. Result: %d\r\n", (int)co2_u32);
@@ -155,7 +146,7 @@ uint32_t CO2_Read(void)
 		sprintf(DataChar_UART,"checkSum Error: CO2=333\r\n");
 		co2_u32 = 333;
 	}
-	HAL_UART_Transmit(&huart1, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
+	HAL_UART_Transmit(MH_DEBUG_UART, (uint8_t *)DataChar_UART, strlen(DataChar_UART), 100);
 
 	cmd_co2_char[icmd_co2_u32++] = 0; // we received whole command, setting end of string
 	icmd_co2_u32 = 0;
